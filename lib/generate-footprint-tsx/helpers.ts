398965cd9aa1f50fcae14a3b6fd136514723b8ad
Anil Kumar <anilkumar90875199@gmail.com>
2026-06-11 13:06:35 +0530

    Support additional pcb_plated_hole variants and properties (#46)
    * Support additional pcb_plated_hole variants and properties
    
    * remove methoda
    
    * remove methoda

--- lib/generate-footprint-tsx/helpers.ts
+++ lib/generate-footprint-tsx/helpers.ts
@@ -13,6 +13,16 @@ export const formatOptionalMmAttr = (
   return ` ${attrName}="${mmStr(value)}"`
 }
 
+export const formatSolderMaskAttrs = (platedHole: {
+  is_covered_with_solder_mask?: boolean
+  soldermask_margin?: number
+}): string =>
+  `${
+    platedHole.is_covered_with_solder_mask === undefined
+      ? ""
+      : ` coveredWithSolderMask={${platedHole.is_covered_with_solder_mask}}`
+  }${formatOptionalMmAttr("solderMaskMargin", platedHole.soldermask_margin)}`
+
 export const formatPcbRotationAttr = (
   rotation: string | number | undefined,
   attrName = "pcbRotation",
