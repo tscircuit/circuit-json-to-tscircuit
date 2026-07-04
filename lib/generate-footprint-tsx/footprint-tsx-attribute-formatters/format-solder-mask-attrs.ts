affb91553497aaa10d2141ca5087116ca155d6d2
anil08607 <anilkumar90875199@gmail.com>
2026-07-04 08:37:15 +0530

    remove tertory

--- lib/generate-footprint-tsx/footprint-tsx-attribute-formatters/format-solder-mask-attrs.ts
+++ lib/generate-footprint-tsx/footprint-tsx-attribute-formatters/format-solder-mask-attrs.ts
@@ -3,9 +3,12 @@ import { formatOptionalMmAttr } from "./format-optional-mm-attr"
 export const formatSolderMaskAttrs = (platedHole: {
   is_covered_with_solder_mask?: boolean
   soldermask_margin?: number
-}): string =>
-  `${
-    platedHole.is_covered_with_solder_mask === undefined
-      ? ""
-      : ` coveredWithSolderMask={${platedHole.is_covered_with_solder_mask}}`
-  }${formatOptionalMmAttr("solderMaskMargin", platedHole.soldermask_margin)}`
+}): string => {
+  let coveredWithSolderMaskAttr = ""
+
+  if (platedHole.is_covered_with_solder_mask !== undefined) {
+    coveredWithSolderMaskAttr = ` coveredWithSolderMask={${platedHole.is_covered_with_solder_mask}}`
+  }
+
+  return `${coveredWithSolderMaskAttr}${formatOptionalMmAttr("solderMaskMargin", platedHole.soldermask_margin)}`
+}
